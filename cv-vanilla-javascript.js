5beb1741fb90ed293506e01a23140c7b8204bb9f
arturs <artyvt@gmail.com>
2021-10-13 00:07:37 +0300

    header created , needs refactoring

--- cv-vanilla-javascript.js
+++ cv-vanilla-javascript.js
@@ -1,5 +1,3 @@
-// helena
-
 document.title = "CV JavaScript";
 
 async function getData() {
@@ -24,10 +22,57 @@ async function createNavbar() {
     li.appendChild(a);
     navbar.appendChild(li);
   });
-
-  document.getElementById("navbar");
-
   document.body.appendChild(navbar);
 }
 
+async function createHeader() {
+  const header = document.createElement("header");
+  const content = document.createElement("div");
+  const image = document.createElement("div");
+
+  let nameTag = document.createElement("h1");
+  nameTag.setAttribute("id", "name");
+
+  let jobTitleTag = document.createElement("h5");
+  jobTitleTag.setAttribute("id", "jun-soft-dev");
+
+  let titleTag = document.createElement("h3");
+  titleTag.setAttribute("id", "about-me");
+
+  let aboutMeTag = document.createElement("p");
+
+  const headerData = await getData();
+
+  let name = document.createTextNode(headerData.header.name);
+  nameTag.appendChild(name);
+
+  let jobTitle = document.createTextNode(headerData.header.jobTitle);
+  jobTitleTag.appendChild(jobTitle);
+
+  let title = document.createTextNode(headerData.header.title);
+  titleTag.appendChild(title);
+
+  const aboutMeData = await fetch("vanilla-js-assets/header-description.txt");
+  let aboutMeTextNode = document.createTextNode(await aboutMeData.text());
+  aboutMeTag.appendChild(aboutMeTextNode);
+
+  const pic = document.createElement("img");
+  pic.setAttribute("id", "my-pic");
+  pic.setAttribute("src", "resources/images/AV_LinkedIn.jpg");
+
+  content.appendChild(nameTag);
+  content.appendChild(jobTitleTag);
+  content.appendChild(titleTag);
+  content.appendChild(aboutMeTag);
+
+  image.appendChild(pic);
+
+  header.appendChild(content);
+  header.appendChild(image);
+
+  document.body.appendChild(header);
+  
+}
+
 createNavbar();
+createHeader();
